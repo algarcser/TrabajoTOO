4c63712a984d1e02545cf15b6dbce03ea99c2221
algarcser <43754393+algarcser@users.noreply.github.com>
2021-01-18 01:22:13 +0100

    base datos vehiculo

--- CapaPersistenciaVehiculo/BDvehiculo.cs
+++ CapaPersistenciaVehiculo/BDvehiculo.cs
@@ -8,5 +8,64 @@ namespace CapaPersistenciaVehiculo
 {
     class BDvehiculo
     {
+
+        private static TablaVehiculo vehiculos;
+
+        //private static TablaExtras extras;
+        private BDvehiculo() { }
+        public static TablaVehiculo Clientes
+        {
+            get
+            {
+                if (vehiculos == null)
+                    vehiculos = new TablaVehiculo();
+                return vehiculos;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="c"></param>
+        public static void INSERTCliente(vehiculoDato c)
+        {
+            BDvehiculo.vehiculos.Add(c);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="l"></param>
+        public static List<vehiculoDato> SELECTCliente()
+        {
+            List<vehiculoDato> lista = new List<vehiculoDato>();
+            return lista;
+        }
+
+        public static vehiculoDato SELECTVehiculo(vehiculoDato c)
+        {
+            return c;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="c"></param>
+        public static void UPDATEVehiculo(vehiculoDato c)
+        {
+            DELETECliente(c);
+            INSERTCliente(c);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="c"></param>
+        public static void DELETECliente(vehiculoDato c)
+        {
+            BDvehiculo.vehiculos.Remove(c);
+        }
+
     }
 }
