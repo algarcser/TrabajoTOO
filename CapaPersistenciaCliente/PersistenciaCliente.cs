cd4f9586c0b77f9b6240a773cdde6b9ca9e6d067
vicoman99 <43779372+vicoman99@users.noreply.github.com>
2021-01-19 12:56:16 +0100

    modificado el conversor de cliente

--- CapaPersistenciaCliente/PersistenciaCliente.cs
+++ CapaPersistenciaCliente/PersistenciaCliente.cs
@@ -48,24 +48,12 @@ namespace CapaPersistenciaCliente
         {
             public static Cliente Convertir(ClienteDato clienteDato)
             {
-                CategoriaCliente categoria;
-                if (clienteDato.getcategoria() == 0) { categoria = CategoriaCliente.A; }
-                else if (clienteDato.getcategoria().Equals(1)) { categoria = CategoriaCliente.B; }
-                else { categoria = CategoriaCliente.C; }
-
-                return new Cliente(clienteDato.getNombre(), clienteDato.getDNI(), categoria, clienteDato.getTlfno());
-   
+                return new Cliente(clienteDato.getNombre(), clienteDato.getDNI(), (CategoriaCliente) clienteDato.getcategoria(), clienteDato.getTlfno());
             }
 
             public static ClienteDato Convertir(Cliente cliente)
             {
-                CategoriaClienteDato categoria;
-                if (cliente.getcategoria() == 0) { categoria = CategoriaClienteDato.A; }
-                else if (cliente.getcategoria().Equals(1)) { categoria = CategoriaClienteDato.B; }
-                else { categoria = CategoriaClienteDato.C; }
-
-                return new ClienteDato(cliente.getNombre(), cliente.getDNI(), categoria, cliente.getTlfno());
-            
+                return new ClienteDato(cliente.getNombre(), cliente.getDNI(), (CategoriaClienteDato) cliente.getcategoria(), cliente.getTlfno());           
             }
         }
     }
