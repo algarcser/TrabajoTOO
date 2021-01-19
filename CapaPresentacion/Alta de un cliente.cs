e8f37cf43286fa885b81b832f2c5a04f4964d1de
vicoman99 <43779372+vicoman99@users.noreply.github.com>
2021-01-19 12:44:22 +0100

    Hecha la parte de presentacion
    solo la apariencia, falta funcionalidad

---
+++ CapaPresentacion/Alta de un cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class Alta_de_un_cliente : UserControl
+    {
+        public Alta_de_un_cliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
