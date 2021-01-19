44396320221f5b170de3528e6ac5dfd9029015af
jugarcca <43774828+jugarcca@users.noreply.github.com>
2021-01-19 12:59:14 +0100

    Commit nuevos proyectos presentación

--- LogicaNegocioPresupuesto/LNPresupuesto.cs
+++ LogicaNegocioPresupuesto/LNPresupuesto.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CapaPersistenciaPresupuesto;
+using LogicaModeloPresupuesto;
 
 namespace LogicaNegocioPresupuesto
 {
-    public class LNPresupuesto
+    class LNPresupuesto
     {
+        public void actualizarEstado(Presupuesto presupuesto, EstadoPresupuesto estado)
+        {
+            if (((int)estado != 0) && ((int)estado != 2))
+            {
+                //presupuesto.FechaRealizacion
+            }
+        }
     }
 }
