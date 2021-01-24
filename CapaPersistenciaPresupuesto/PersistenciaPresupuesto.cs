249d6678454526bac757fd7db4632fa3632c1e97
jugarcca <43774828+jugarcca@users.noreply.github.com>
2021-01-24 02:27:56 +0100

    Corrección SELECTALL

--- CapaPersistenciaPresupuesto/PersistenciaPresupuesto.cs
+++ CapaPersistenciaPresupuesto/PersistenciaPresupuesto.cs
@@ -68,7 +68,7 @@ namespace CapaPersistenciaPresupuesto
                 listaP.Add(conversor.Convertir(p));
             }
 
-            return (listaPD);
+            return (listaP);
         }
 
         public static class conversor
