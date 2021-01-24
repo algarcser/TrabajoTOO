a44971a6d64b6ab9ae3720874c8e3fe8e365f6d2
algarcser <43754393+algarcser@users.noreply.github.com>
2021-01-24 23:59:31 +0100

    bases de datos de extras y extras vehiculo

---
+++ CapaPersistenciaVehiculo/BDExtras.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaPersistenciaVehiculo
+{
+    internal class BDExtras
+    {
+        private static TablaExtras tablaExtras;
+
+        private BDExtras() { }
+
+        private static TablaExtras TablaExtras
+        {
+            get
+            {
+                if(tablaExtras == null)
+                {
+                    tablaExtras = new TablaExtras();
+                }
+                return tablaExtras;
+            }
+        }
+
+
+
+        internal static void INSERT(extraDato c)
+        {
+            BDExtras.TablaExtras.Add(c);
+        }
+
+
+
+        internal static bool SELECT(extraDato referencia, out extraDato extraDato)
+        {
+
+            if (BDExtras.Exists(referencia))
+            {
+                extraDato = BDExtras.TablaExtras[referencia.Id];
+                return true;
+            }
+            else
+            {
+                extraDato = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="c"></param>
+        internal static void UPDATE(extraDato c)
+        {
+            DELETE(c);
+            INSERT(c);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="c"></param>
+        internal static void DELETE(extraDato c)
+        {
+            BDExtras.TablaExtras.Remove(c);
+        }
+
+        internal static bool Exists(extraDato extraDato)
+        {
+            return BDExtras.TablaExtras.Contains(extraDato.Id);
+        }
+
+        internal static List<extraDato> SELECT_ALL()
+        {
+            List<extraDato> lista = new List<extraDato>();
+            foreach (extraDato extraDato in BDExtras.TablaExtras)
+            {
+                lista.Add(extraDato);
+            }
+            return lista;
+        }
+    }
+}
---
+++ CapaPersistenciaVehiculo/BDVehiculo_Extras.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaPersistenciaVehiculo
+{
+    class BDVehiculo_Extras
+    {
+        private static Tabla_VehiculoNuevo_Extras tabla_vehiculoNuevo_extras;
+
+        internal BDVehiculo_Extras() { }
+
+        internal static Tabla_VehiculoNuevo_Extras Tabla_vehiculoNuevo_extras
+        {
+            get
+            {
+                if(tabla_vehiculoNuevo_extras == null)
+                {
+                    tabla_vehiculoNuevo_extras = new Tabla_VehiculoNuevo_Extras();
+                }
+                return tabla_vehiculoNuevo_extras;
+            }
+        }
+
+        internal static void INSERT(vehiculoNuevo_Extra_Dato c)
+        {
+            BDVehiculo_Extras.Tabla_vehiculoNuevo_extras.Add(c);
+        }
+
+
+
+        internal static bool SELECT(vehiculoNuevo_Extra_Dato referencia, out vehiculoNuevo_Extra_Dato vehiculoNuevo_Extra_Dato)
+        {
+
+            if (BDVehiculo_Extras.Exists(referencia))
+            {
+                vehiculoNuevo_Extra_Dato = BDVehiculo_Extras.Tabla_vehiculoNuevo_extras[referencia.Key];
+                return true;
+            }
+            else
+            {
+                vehiculoNuevo_Extra_Dato = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="c"></param>
+        internal static void UPDATE(vehiculoNuevo_Extra_Dato c)
+        {
+            DELETE(c);
+            INSERT(c);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="c"></param>
+        internal static void DELETE(vehiculoNuevo_Extra_Dato c)
+        {
+            BDVehiculo_Extras.Tabla_vehiculoNuevo_extras.Remove(c);
+        }
+
+        internal static bool Exists(vehiculoNuevo_Extra_Dato vehiculoNuevo_Extra_Dato)
+        {
+            return BDVehiculo_Extras.Tabla_vehiculoNuevo_extras.Contains(vehiculoNuevo_Extra_Dato.Key);
+        }
+
+        internal static List<vehiculoNuevo_Extra_Dato> SELECT_ALL()
+        {
+            List<vehiculoNuevo_Extra_Dato> lista = new List<vehiculoNuevo_Extra_Dato>();
+            foreach (vehiculoNuevo_Extra_Dato vehiculo_extra in BDVehiculo_Extras.Tabla_vehiculoNuevo_extras)
+            {
+                lista.Add(vehiculo_extra);
+            }
+            return lista;
+        }
+
+    }
+}
