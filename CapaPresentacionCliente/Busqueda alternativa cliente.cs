724fbaca81b425ca4ffdc547572556a776cd13b7
vicoman99 <43779372+vicoman99@users.noreply.github.com>
2021-01-26 23:41:32 +0100

    acabada la busqueda alternativa del cliente

--- CapaPresentacionCliente/Busqueda alternativa cliente.cs
+++ CapaPresentacionCliente/Busqueda alternativa cliente.cs
@@ -17,11 +17,18 @@ namespace CapaPresentacionCliente
         public Busqueda_alternativa_cliente(Cliente c)
         {
             InitializeComponent();
-
+            // Lo que hago aqui es mostrar el cliente que busca en el formulario de introducir DNI
             Cliente clBuscado = LNCliente.readCliente(c);
-
             this.textBoxNombre.Text = clBuscado.getNombre;
             //this.textBoxApellidos.Text = clBuscado.getApellidos;
+
+            // Y aqui dar la opcion de mostrar otros del comboBox
+            List<string> listaAux = LNCliente.SELECT_ALL().Select(x => x.getDNI).ToList();
+            foreach(string elem in listaAux)
+            {
+                this.comboBox1.Items.Add(elem);
+            }
+
         }
 
         private void btCerrar_Click(object sender, EventArgs e)
